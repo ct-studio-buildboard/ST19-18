5b7cb6e6cb617f55d385c4f671273ede421f3361
Roger Wang <rw575@cornell.edu>
2019-04-22 22:12:08 -0400

    add debug output

--- docs/js/update_speed.js
+++ docs/js/update_speed.js
@@ -10,6 +10,7 @@ function get_speed() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+    	console.log(this.responseText)
       return this.responseText;
     }
   };
